10f785f23068e0605469ab3892a62ffeeacb3d69
August <coop@coopeeo.xyz>
2026-08-05 13:00:17 -0500

    change how it makes fonts because better

--- font/setup-font.py
+++ font/setup-font.py
@@ -1,30 +1,70 @@
 import shutil
 
 OUT_DIR = "setup"
+GLYPHS = [ # the file name will be a ligature, aliases will be additional ones
+    {
+        "file": "rovalra.svg",
+    },
+    {
+        "file": "square.svg",
+    },
+    {
+        "file": "borderless.svg",
+    },
+    {
+        "file": "circle.svg",
+    },
+    {
+        "file": "icon.svg",
+        "aliases": [
+            "contributor"
+        ]
+    },
+    {
+        "file": "bronze.svg",
+    },
+    {
+        "file": "silver.svg",
+    },
+    {
+        "file": "gold.svg",
+    },
+    {
+        "file": "diamond.svg",
+    },
+]
 
 def main():
     currentPUA = 0xe000
-    fontGlyphs = [ # name is what the ligature will be
-        {"name": "rovalra", "file": "rovalra.svg"},
-        {"name": "square", "file": "square.svg"},
-        {"name": "borderless", "file": "borderless.svg"},
-        {"name": "circle", "file": "circle.svg"},
-        {"name": "icon", "file": "icon.svg"},
-        {"name": "bronze", "file": "bronze.svg"},
-        {"name": "silver", "file": "silver.svg"},
-        {"name": "gold", "file": "gold.svg"},
-        {"name": "diamond", "file": "diamond.svg"},
-    ]
 
-    for glyph in fontGlyphs:
-        ligature = []
+    for glyph in GLYPHS:
 
-        for char in glyph['name']:
-            ligature.append(str(hex(ord(char))).lstrip('0x'))
+        # Unicode PUA charactor
 
-        shutil.copyfile(src=glyph['file'], dst=f'setup/{str(hex(currentPUA)).lstrip('0x')}.svg')
-        shutil.copyfile(src=glyph['file'], dst=f'setup/{'_'.join(ligature)}.svg')
+        shutil.copyfile(src=glyph['file'], dst=f'{OUT_DIR}/{str(hex(currentPUA)).lstrip('0x')}.svg')
+
         currentPUA += 0x1
+
+        # Main ligature based on filename
+        mainLigature = []
+
+        for char in glyph['file'].rstrip('.svg'):
+            mainLigature.append(str(hex(ord(char))).lstrip('0x'))
+
+        shutil.copyfile(src=glyph['file'], dst=f'{OUT_DIR}/{'_'.join(mainLigature)}.svg')
+
+        # Additional ligature aliases
+        if glyph['aliases']:
+            for alias in glyph['aliases']:
+                aliasLigature = []
+
+                for char in alias:
+                    aliasLigature.append(str(hex(ord(char))).lstrip('0x'))
+
+                shutil.copyfile(src=glyph['file'], dst=f'{OUT_DIR}/{'_'.join(aliasLigature)}.svg')
+
+
+
 
 
 if __name__ == "__main__":
