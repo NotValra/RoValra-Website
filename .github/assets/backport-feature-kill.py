ed38588b52034ca0fa10a04324857259eb4a2260
August <coop@coopeeo.xyz>
2026-09-22 10:22:33 -0500

    try to add some stuff

--- .github/assets/backport-feature-kill.py
+++ .github/assets/backport-feature-kill.py
@@ -10,6 +10,16 @@ feature_kill_backport = {}
 with open(FEATURE_KILL_FILE, 'r', encoding='utf-8') as f:
     feature_kill_contents = json.load(f)
 
+for quickFeat in feature_kill_contents.get("quick", []):
+    feature_kill_contents.get("features", []).append({
+        "setting": quickFeat,
+        "incompatibilities": [
+                {
+                    "versions": "*",
+                }
+        ]
+    })
+
 
 for feat in feature_kill_contents.get("features", []):
     if feat.get("incompatibilities") and len(feat["incompatibilities"]) > 0:
@@ -21,3 +31,6 @@ os.makedirs(os.path.dirname(FEATURE_KILL_BACKPORT_FILE), exist_ok=True)
 
 with open(FEATURE_KILL_BACKPORT_FILE, 'w', encoding='utf-8') as f:
     json.dump(feature_kill_backport, f, indent=4)
+
+with open(FEATURE_KILL_FILE, 'w', encoding='utf-8') as f:
+    json.dump(feature_kill_contents, f, indent=4)
